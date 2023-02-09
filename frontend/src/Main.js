2d5655ba87f9c72d15647489a433782606abcf19
Ben Radford <benradf@users.noreply.github.com>
2023-02-09 21:03:23 +0000

    Fix scrollIntoView on svg element issue in Firefox.

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,6 +1,10 @@
 export function scrollIntoView(element) {
   return function () {
-    element.scrollIntoView({
+    const documentElement = document.documentElement;
+    const elementBounds = element.getBoundingClientRect();
+    const top = window.pageYOffset + elementBounds.top - documentElement.clientHeight / 2 + elementBounds.height / 2;
+    const left = window.pageXOffset + elementBounds.left - documentElement.clientWidth / 2 + elementBounds.width / 2;
+    window.scrollTo({
       behavior: "smooth",
       block: "center",
       inline: "center",
