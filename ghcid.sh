674f2140685a18d51351f06a81e9278c6f86da19
Ben Radford <benradf@users.noreply.github.com>
2023-03-01 13:39:02 +0000

    Make ghcid wrapper script work with nix flakes.

--- ghcid.sh
+++ ghcid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nix-shell --run '
-    LC_ALL=C.UTF-8 ghcid --allow-eval --command "
-        bazel run //backend:skyscope@repl
-    "
-'
+LC_ALL=C.UTF-8 nix \
+  --extra-experimental-features flakes \
+  --extra-experimental-features nix-command \
+  develop --command ghcid --allow-eval --command \
+    bazel run //backend:skyscope@repl
