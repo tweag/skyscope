46935ee4a3f123815ff9e030209425959f3eb96c
Ben Radford <benradf@users.noreply.github.com>
2023-03-20 13:15:38 +0000

    Update documentation and remove old wrapper script.

--- demo.sh
+++ demo.sh
@@ -3,4 +3,13 @@ set -eux
 nix develop \
   --extra-experimental-features flakes \
   --extra-experimental-features nix-command \
-  --command bash -c "bazel build //backend:skyscope && '$PWD/bin/skyscope'"
+  --command bash -c '
+    bazel build //:skyscope
+
+    # The entire graph takes too long to import for a demo,
+    # so clear it and then populate a smaller subgraph.
+    bazel shutdown
+    bazel build //backend:skyscope
+
+    bazel-bin/skyscope --query --aquery
+  '
