a0014be0b717c9c9a8f7731f637f13a6acafc3b7
Ben Radford <benradf@users.noreply.github.com>
2023-03-31 10:56:30 +0100

    Remove case that formats artifact node titles as depset.

--- frontend/src/format.js
+++ frontend/src/format.js
@@ -7,9 +7,6 @@ const truncate = (content, n) => {
 const title = function () {
     var match = null;
     switch (node.type) {
-        case "Artifact":
-        case "ArtifactNestedSet":
-            return "depset";
         case "ConfiguredTarget":
         case "TransitiveTarget":
         case "TargetCompletion":
