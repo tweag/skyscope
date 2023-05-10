ae495310451d2377b232cd67db8a7f5e53f2cf48
Ben Radford <benradf@users.noreply.github.com>
2023-05-10 08:35:47 +0100

    Print example of a graph in json format.

--- backend/src/Model.hs
+++ backend/src/Model.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Model where
 
 import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.ByteString.Lazy.Char8 as LBSC
 import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
@@ -34,3 +38,14 @@ data Edge = Edge
   deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
 
 type Graph = (NodeMap Node, Set Edge)
+
+exampleGraph :: Graph
+exampleGraph =
+  ( Map.fromList
+      [ ("hash1", (Node "data1" "type1")),
+        ("hash2", (Node "data2" "type2"))
+      ],
+    Set.fromList [Edge 0 "hash1" "hash2"]
+  )
+
+-- $> LBSC.putStrLn $ Data.Aeson.encode Model.exampleGraph
