55da7d30fdf52c8c9ac3a736d4a75c33e260023a
Ben Radford <benradf@users.noreply.github.com>
2023-10-19 13:33:40 +0100

    Index edge table by target node to fix slow query.

--- backend/src/schema.sql
+++ backend/src/schema.sql
@@ -16,6 +16,8 @@ CREATE TABLE IF NOT EXISTS edge (
   FOREIGN KEY (target) REFERENCES node(idx)
 );
 
+CREATE INDEX IF NOT EXISTS target_index ON edge (target);
+
 CREATE TABLE IF NOT EXISTS path (
   destination INTEGER,
   steps BLOB,
